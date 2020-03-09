3ead868e32762d7404523832e1eab4e9af7a96a3
shannoncascaddan <shannoncascaddan@gmail.com>
2020-03-09 13:29:02 -0700

    Added succulents

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -5,5 +5,6 @@ public class treeBranchTrunk
         System.out.println("the trees are in bloom");
         System.out.println("Mr. Beast planted 20,000,000 trees");
         System.out.println("There are many types of trees");
+        System.out.println("Succulents are nice");
     }
 }
